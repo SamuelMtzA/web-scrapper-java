133a8066c836af662692fcc30a49b788e3e41591
Samuel Martinez Arreola <sma.2904@gmail.com>
2022-09-25 22:39:21 -0500

    When one thread is executing a synchronized method for an object, all other threads that invoke synchronized methods for the same object block (suspend execution) until the first thread is done with the object.

--- src/scrapper/Main.java
+++ src/scrapper/Main.java
@@ -14,22 +14,36 @@ import java.util.stream.Stream;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    String linkPage = "https://www.bbc.com/";
-    getWebContent(linkPage);
+    // String linkPage = "https://www.bbc.com/";
+    // getWebContent(linkPage);
+
+    List<String> webPages = new ArrayList<>();
+    webPages.add("https://www.bbc.com/news/world");
+    webPages.add("https://www.bbc.com/news/uk");
+    webPages.add("https://www.bbc.com/news/business");
+    webPages.add("https://www.bbc.com/news/technology");
+    webPages.add("https://www.bbc.com/news/entertainment_and_arts");
+    // webPages.stream().forEach(page -> getWebContent(page));
+    webPages.stream().parallel().forEach(page -> getWebContent(page));
+    
+
   }
   //Downloads web pages, READ FILES OR PROCESS DATA HERE
-  private static String getWebContent (String url) {
+  private static synchronized String getWebContent (String url) {
+    System.out.println("Downloading: " + url);
     try {
       URL newURL = new URL(url);
       HttpURLConnection httpConnection = (HttpURLConnection) newURL.openConnection();
       String encoding = httpConnection.getContentEncoding();
   
       InputStream input = httpConnection.getInputStream();
+
       Stream<String> lines = new BufferedReader( new InputStreamReader(input) ).lines();
       System.out.println("End");
 
       String result = lines.collect(Collectors.joining());
-      System.out.println(result);
+      // System.out.println(result);
+      System.out.println("End");
       return result;
       
     } catch (Exception e) {
